86da390d00ec579802fcf0967da7a2285f6f1bc1
DESKTOP-9TSUMFC\75176 <751768390@qq.com>
2023-02-09 19:19:40 +0800

    开始首页编写

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>
-    Home-View
-  </h1>
+  <div class="home lazy-animate-init">
+    <div class="carousel-wrap">
+      <div class="swiper-container banner-container swiper-container-initialized swiper-container-horizontal"></div>
+      <div class="carousel-preview"></div>
+    </div>
+  </div>
 </template>
 <script setup>
 </script>
 <style scoped>
+.home {
+  position: relative;
+  min-height: 100vh;
+}
+
+.lazy-animate-init {
+  -webkit-transition: opacity .5s linear, -webkit-transform .5s ease;
+  transition: transform .5s ease, opacity .5s linear;
+  visibility: visible;
+  opacity: 1;
+  -webkit-transform: none;
+  transform: none;
+}
+
+.carousel-wrap {
+  position: relative;
+  width: 100%;
+}
 </style>
