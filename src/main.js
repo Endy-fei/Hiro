d6ec14c4aed14e0bc87223daf7f3fcdc89cc6c34
DESKTOP-9TSUMFC\75176 <751768390@qq.com>
2023-02-10 17:35:17 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+
+// import function to register Swiper custom elements
+import {register} from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
+
+
 import './assets/main.css'
+
 
 const app = createApp(App)
 
