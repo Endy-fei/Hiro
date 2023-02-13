4d2a193343cb5266f447cc5f36db58ba2bbdc122
DESKTOP-9TSUMFC\75176 <751768390@qq.com>
2023-02-13 17:45:22 +0800

    修改

--- src/views/home_components/ProductSwiper.vue
+++ src/views/home_components/ProductSwiper.vue
@@ -12,12 +12,103 @@
       </div>
       <div class="line-right"></div>
     </div>
-    <div class="product-carousel-wrap"></div>
+    <div class="swiper-my-pagination"></div>
+    <div class="product-carousel-wrap">
+      <div style="display: flex">
+        <div class="swiper-button-prev"></div>
+        <swiper :modules="modules" :slides-per-view="1" :space-between="0" :navigation="navigation"
+                :autoplay="autoplay"
+                :pagination="pagination" :scrollbar="false" :loop="true" @swiper="onSwiper"
+                @slideChange="onSlideChange">
+
+          <swiper-slide class="carousel-img" slide-1>
+            <img src="@/assets/images/2/2-123.png" alt="2-123">
+          </swiper-slide>
+          <swiper-slide class="carousel-img" slide-2>
+            <img src="@/assets/images/2/3-123.png" alt="3-123">
+          </swiper-slide>
+          <swiper-slide class="carousel-img" slide-3>
+            <img src="@/assets/images/2/4-123.png" alt="4-123">
+          </swiper-slide>
+          <swiper-slide class="carousel-img" slide-3>
+            <img src="@/assets/images/2/5-123.png" alt="5-123">
+          </swiper-slide>
+          <swiper-slide class="carousel-img" slide-3>
+            <img src="@/assets/images/2/6-123.png" alt="6-123">
+          </swiper-slide>
+        </swiper>
+        <div class="swiper-button-next"></div>
+      </div>
+    </div>
   </div>
 
 </template>
 
 <script setup>
+import {A11y, Autoplay, Navigation, Pagination, Scrollbar} from "swiper";
+import {Swiper, SwiperSlide} from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import {onMounted} from "vue";
+
+const modules = [Navigation, Pagination, Scrollbar, A11y, Autoplay];
+
+const navigation = {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+};
+
+const autoplay = {
+  delay: 3000,
+  disableOnInteraction: false,
+};
+
+const pagination = {
+  el: '.swiper-my-pagination',
+  bulletElement: 'span',
+  clickable: true,
+  type: 'custom',
+  renderCustom: function (swiper, current, total) {
+    let paginationHtml = '';
+    const text = ['调音台', '音响', '服务器', '软件支持', '直播服务']
+    for (let i = 1; i <= total; i++) {
+      if (i === current) {
+        // language=HTML format=true
+        paginationHtml += `
+          <div class='pagination pagination_active'>
+            <span class='pagination_text'>${text[i - 1]}</span>
+          </div>`
+      } else {
+        // language=HTML format=true
+        paginationHtml += `
+          <div class='pagination'>
+            <span class='pagination_text'>${text[i - 1]}</span>
+          </div>`
+      }
+    }
+    return paginationHtml
+  }
+};
+
+onMounted(() => {
+
+});
+
+
+let swiperProduct = undefined;
+const onSwiper = (s) => {
+  console.log('swiper');
+  swiperProduct = s
+};
+
+const onSlideChange = (s) => {
+  console.log('slide change');
+  console.log(s.realIndex);
+};
+
 </script>
 
 <style scoped>
@@ -83,5 +174,48 @@
   background-color: #000;
   margin-left: 4px;
 }
+
+.product-carousel-wrap {
+  width: 1200px;
+  margin: 0 auto;
+}
+
+.swiper-wrapper .carousel-img {
+  width: 938px;
+  font-size: 0;
+}
+
+.swiper-wrapper .carousel-img img {
+  width: 100%;
+}
+
+.product-carousel-wrap .swiper-button-next, .product-carousel-wrap .swiper-button-prev {
+  position: unset;
+  margin: 0 54px 212px;
+  height: auto;
+  outline: 0;
+  color: #45454d;
+}
+
+.swiper-my-pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 0;
+  bottom: 10px;
+  left: 0;
+  width: 100%;
+}
+
+/*.swiper-my-pagination的子元素 */
+
+
+.swiper-my-pagination > pagination {
+  margin: 0 12px !important;
+  background: none;
+  font-weight: 600;
+  color: #000;
+}
+
 
 </style>
