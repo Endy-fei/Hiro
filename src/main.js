a916fdd4d0e9386558d80bb3f4626ee97184e01f
tcb <tcb@tencent.com>
2023-03-07 08:47:57 +0000

    init

--- src/main.js
+++ src/main.js
@@ -1,22 +1,4 @@
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
-
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-
-// import function to register Swiper custom elements
-import {register} from 'swiper/element/bundle';
-// register Swiper custom elements
-register();
-
-
-import './assets/main.css'
-
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
