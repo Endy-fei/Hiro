b04f1d3cdbdb8bea63eac480e50919a2416b8901
DESKTOP-9TSUMFC\75176 <751768390@qq.com>
2023-03-07 17:13:45 +0800

    腾讯云兼容性配置

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [vue()],
+    resolve: {
+        // 配置路径别名
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    // resolve: {
+    //   alias: {
+    //     '@': fileURLToPath(new URL('./src', import.meta.url))
+    //   }
+    // }
 })
